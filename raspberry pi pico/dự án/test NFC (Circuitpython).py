1b37ab315d97e4b5434086e4c14fb613bd55d247
Đinh Viết Phúc <159451818+vodanh404@users.noreply.github.com>
2025-06-28 14:46:33 +0700

    Create Test NFC (Circuitpython).py

---
+++ raspberry pi pico/dự án/test NFC (Circuitpython).py
@@ -0,0 +1,27 @@
+import board
+import time
+from mfrc522 import MFRC522
+# thiết lập kết nối
+sck = board.GP2
+mosi = board.GP3
+miso = board.GP4
+cs = board.GP5
+rst = board.GP28
+###
+rfid = MFRC522(sck, mosi, miso, cs, rst)
+prev_data = None
+while True:
+    (status, tag_type) = rfid.request(rfid.REQALL)
+
+    if status == rfid.OK:
+        print("Card detected, tag type:", tag_type)
+
+        (status, raw_uid) = rfid.anticoll()
+
+        if status == rfid.OK:
+            rfid_data = "{:02x}{:02x}{:02x}{:02x}".format(*raw_uid)
+            if rfid_data != prev_data:
+                prev_data = rfid_data
+                print("Card UID:", rfid_data)
+
+    time.sleep(0.2)
